d7006f046fcf7e78e48524bb4e7710541d306093
Pietro Di Bello <pierodibello@gmail.com>
2019-12-29 18:03:34 +0100

    fix response

--- functions/get-index.js
+++ functions/get-index.js
@@ -10,13 +10,14 @@ function* loadHtml() {
   if (!html)  {
     html = yield fs.readFileAsync('static/index.html', 'UTF-8')
   }
+
   return html
 }
 
 module.exports.handler = co.wrap(function* (event, context, callback) {
   let html = yield loadHtml()
   const response = {
-    status: 200,
+    statusCode: 200,
     body: html,
     headers: {
       'Content-Type': 'text/html; charset=UTF-8'
