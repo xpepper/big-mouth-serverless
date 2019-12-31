e09ec36acff3278004d981b9f017140fb989dd25
Pietro Di Bello <pierodibello@gmail.com>
2019-12-31 19:25:19 +0100

    render landing page with current day of week and an empty list of restaurants

--- functions/get-index.js
+++ functions/get-index.js
@@ -3,8 +3,14 @@
 const co = require("co")
 const Promise = require("bluebird")
 const fs = Promise.promisifyAll(require("fs"))
+const Mustache = require("mustache")
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 var html
+
+function* getRestaurants() {
+  return []
+}
 
 function* loadHtml() {
   if (!html)  {
@@ -15,7 +21,11 @@ function* loadHtml() {
 }
 
 module.exports.handler = co.wrap(function* (event, context, callback) {
-  let html = yield loadHtml()
+  let template = yield loadHtml()
+  let restaurants = yield getRestaurants()
+  let dayOfWeek = days[new Date().getDay()]
+  let html = Mustache.render(template, { dayOfWeek, restaurants })
+
   const response = {
     statusCode: 200,
     body: html,
