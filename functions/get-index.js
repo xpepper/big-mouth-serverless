56f2499d46413f7d55d10b333e4bd7661143bb3f
Pietro Di Bello <pierodibello@gmail.com>
2020-01-06 17:11:51 +0100

    integrate cognito authentication logic into the index page, to sign in and be able to issue a search on restaurants

--- functions/get-index.js
+++ functions/get-index.js
@@ -8,6 +8,10 @@ const http = require("superagent-promise")(require("superagent"), Promise)
 const aws4 = require("aws4")
 const URL = require("url")
 
+const awsRegion = process.env.AWS_REGION
+const cognitoUserPoolId = process.env.cognito_user_pool_id
+const cognitoClientId = process.env.cognito_client_id
+
 const restaurantsApiRoot = process.env.restaurants_api
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
@@ -42,7 +46,15 @@ module.exports.handler = co.wrap(function* (event, context, callback) {
   let template = yield loadHtml()
   let restaurants = yield getRestaurants()
   let dayOfWeek = days[new Date().getDay()]
-  let html = Mustache.render(template, { dayOfWeek, restaurants })
+  let view = { 
+    dayOfWeek, 
+    restaurants,
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    searchUrl: `${restaurantsApiRoot}/search`
+  }
+  let html = Mustache.render(template, view)
 
   const response = {
     statusCode: 200,
