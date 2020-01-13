2238ec7d3c10ecec5c20f7d5f4709b87bb14f4e9
Pietro Di Bello <pietro.dibello@xpeppers.com>
2020-01-13 22:12:27 +0100

    use the awscred library to load the AWS credentials needed by the integration test

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,6 +1,8 @@
 'use strict'
 
-const co = require("co")
+const co = require('co')
+const Promise = require('bluebird')
+const awscred = Promise.promisifyAll(require('awscred'))
 
 let initialized = false
 
@@ -14,6 +16,13 @@ let init = co.wrap(function* () {
   process.env.AWS_REGION = "eu-west-1"
   process.env.cognito_client_id = "test_cognito_client_id" 
   process.env.cognito_user_pool_id = "test_cognito_user_pool_id"
+
+  let awsCredentials = (yield awscred.loadAsync()).credentials
+
+  process.env.AWS_ACCESS_KEY_ID = awsCredentials.accessKeyId
+  process.env.AWS_SECRET_ACCESS_KEY = awsCredentials.secretAccessKey
+
+  console.log("AWS credentials loaded")
 
   initialized = true
 })
