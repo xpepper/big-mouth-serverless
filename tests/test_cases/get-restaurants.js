eb21bfd7f9fdc34fd9b5811a8ff2ff661f47f33a
Pietro Di Bello <pietro.dibello@xpeppers.com>
2020-01-13 22:34:25 +0100

    remove trailing ; on all lines of an integration test

--- tests/test_cases/get-restaurants.js
+++ tests/test_cases/get-restaurants.js
@@ -1,24 +1,24 @@
-'use strict';
+'use strict'
 
-const co = require('co');
-const expect = require('chai').expect;
-const init = require('../steps/init').init;
-const when = require('../steps/when');
+const co = require('co')
+const expect = require('chai').expect
+const init = require('../steps/init').init
+const when = require('../steps/when')
 
 describe(`When we invoke the GET /restaurants endpoint`, co.wrap(function* () {
   before(co.wrap(function* () {
-    yield init();
-  }));
+    yield init()
+  }))
 
   it(`Should return an array of 8 restaurants`, co.wrap(function* () {
-    let response = yield when.we_invoke_get_restaurants();
+    let response = yield when.we_invoke_get_restaurants()
 
-    expect(response.statusCode).to.equal(200);
-    expect(response.body).to.have.lengthOf(8);
+    expect(response.statusCode).to.equal(200)
+    expect(response.body).to.have.lengthOf(8)
 
     for (let restaurant of response.body) {
-      expect(restaurant).to.have.property('name');
-      expect(restaurant).to.have.property('image');
+      expect(restaurant).to.have.property('name')
+      expect(restaurant).to.have.property('image')
     }
-  }));
-}));
+  }))
+}))
