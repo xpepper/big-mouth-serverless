36679bfcf3ca942edbda273a2ffb3981c8538852
Pietro Di Bello <pietro.dibello@xpeppers.com>
2020-01-13 22:29:35 +0100

    add two more integration tests for get-restaurants and search-restaurants

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,10 +1,10 @@
-'use_strict'
+'use strict'
 
 const APP_ROOT = '../../'
 const _ = require('lodash')
 
-let we_invoke_get_index = function () {
-  let handler = require(`${APP_ROOT}/functions/get-index`).handler
+function viaHandler(event, functionName) {
+  let handler = require(`${APP_ROOT}/functions/${functionName}`).handler
 
   return new Promise((resolve, reject) => {
     let context = {}
@@ -21,10 +21,21 @@ let we_invoke_get_index = function () {
       }
     }
 
-    handler({}, context, callback)
+    handler(event, context, callback)
   })
 }
 
+let we_invoke_get_index = () => viaHandler({}, 'get-index')
+let we_invoke_get_restaurants = () => viaHandler({}, 'get-restaurants')
+let we_invoke_search_restaurants = theme => {
+  let event = {
+    body: JSON.stringify({ theme })
+  }
+  return viaHandler(event, 'search-restaurants')
+}
+
 module.exports = {
-  we_invoke_get_index
+  we_invoke_get_index,
+  we_invoke_get_restaurants,
+  we_invoke_search_restaurants
 }
