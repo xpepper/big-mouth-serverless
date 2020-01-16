f618999e1a4a1ddff57805572a5e43675c25fbbb
Pietro Di Bello <pietro.dibello@xpeppers.com>
2020-01-16 23:28:21 +0100

    remove the test user created in tests

---
+++ tests/steps/tearDown.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const co = require('co')
+const AWS = require('aws-sdk')
+
+AWS.config.region = 'eu-west-1'
+const cognito = new AWS.CognitoIdentityServiceProvider()
+
+let an_authenticated_user = function* (user) {
+  let req = {
+    UserPoolId: process.env.cognito_user_pool_id,
+    Username: user.username
+  }
+  yield cognito.adminDeleteUser(req).promise()
+
+  console.log(`[${user.username}] - user deleted`)
+}
+
+module.exports = {
+  an_authenticated_user
+}
--- tests/test_cases/search-restaurants.js
+++ tests/test_cases/search-restaurants.js
@@ -5,12 +5,17 @@ const expect = require('chai').expect
 const init = require('../steps/init').init
 const when = require('../steps/when')
 const given = require('../steps/given')
+const tearDown = require('../steps/tearDown')
 
 describe(`Given an authenticated user`, co.wrap(function* () {
   let user
   before(co.wrap(function* () {
     yield init()
     user = yield given.an_authenticated_user()
+  }))
+
+  after(co.wrap(function* () {
+    yield tearDown.an_authenticated_user(user)
   }))
 
   describe(`When we invoke the POST /restaurants/search endpoint with theme 'cartoon'`, co.wrap(function* () {
